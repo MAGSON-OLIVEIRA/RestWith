51c78fc62a3021af9fd926cebd799b067b5717fa
MAGSON-OLIVEIRA <magsond@gmail.com>
2021-07-02 13:43:34 -0300

    values insert

--- RestWith/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
+++ RestWith/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -1,3 +1,3 @@
 ﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
 	('magson', 'Magson', '$2a$16$9qr2tv0HmXbHBsx.TZFjfux742wCZM32a8Wi6iBqwIqaizlHPuxHS', b'1', b'1', b'1', b'1'),
-	('admin', 'Admin', '$2a$16$h4yDQCYTy62R6xrtFDWONeMH3Lim4WQuU/aj8hxW.dJJoeyvtEkhK', b'1', b'1', b'1', b'1');
+	('admin', 'Admin 123', '$2a$16$IbZ2V6Q0/dIHD0j4uprHxesxf6TE1wqm/7fesyh1LZFeHm8TOAUKS', b'1', b'1', b'1', b'1');
